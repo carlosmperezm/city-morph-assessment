3ffc1860e333c616b490d0089ea9963ad3402797
CarlosPerez <carlosmperez2901@gmail.com>
2026-01-30 11:36:22 -0800

    fix: Does not redirect when user is unauthenticated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { getCurrentUser } from "aws-amplify/auth";
+import { getCurrentUser, type AuthUser } from "aws-amplify/auth";
 import DashboardPage from "./pages/DashboardPage";
 import { LoginPage } from "./pages/LoginPage";
 import { RouteNotExistsPage } from "./pages/RouteNotExistsPage";
 import { ErrorPage } from "./pages/ErrorPage";
+import SignUpPage from "./pages/signUpPage";
 
 function App() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
@@ -14,7 +15,8 @@ function App() {
   useEffect(() => {
     async function getUser() {
       try {
-        getCurrentUser();
+        const user: AuthUser = await getCurrentUser();
+        console.log("User found: ", user);
         setIsAuthenticated(true);
         setIsLoading(false);
       } catch {
@@ -28,6 +30,9 @@ function App() {
   if (isLoading) {
     return <h1>Loading</h1>;
   }
+
+  console.log("isAuthenticated:", isAuthenticated);
+  console.log("isLoading:", isLoading);
 
   return (
     <BrowserRouter>
@@ -48,6 +53,11 @@ function App() {
             <Route
               path="/login"
               element={<LoginPage />}
+              errorElement={<ErrorPage />}
+            />
+            <Route
+              path="/signup"
+              element={<SignUpPage />}
               errorElement={<ErrorPage />}
             />
           </>
