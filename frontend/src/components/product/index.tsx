be51ede5fe5c9875197272252a7ca19d489356c2
CarlosPerez <carlosmperez2901@gmail.com>
2026-02-04 15:26:13 -0800

    Show further info when user is admin

--- frontend/src/components/product/index.tsx
+++ frontend/src/components/product/index.tsx
@@ -5,12 +5,26 @@ import styles from "./styles.module.css";
 export default function Product({
   product,
   signedUrl,
+  user,
 }: ProductProps): JSX.Element {
+  const isUserAdmin = user["custom:role"] === "admin";
+
   return (
     <div className={styles.product}>
       <img src={signedUrl} alt={product.name} className={styles.image} />
-      <p className="product-name">Name: {product.name}</p>
-      <p className="product-price">Price: ${product.price}</p>
+      {isUserAdmin ? (
+        <div className={styles.productDetails}>
+          <p>Name: {product.name}</p>
+          <p>Product Id: {product.id}</p>
+          <p>Price: ${product.price}</p>
+          <p>S3 key: {product.image_key}</p>
+        </div>
+      ) : (
+        <div className={styles.productDetails}>
+          <p>Name: {product.name}</p>
+          <p>Price: ${product.price}</p>
+        </div>
+      )}
     </div>
   );
 }
